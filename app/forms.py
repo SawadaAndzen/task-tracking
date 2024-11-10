eec1f9cacd23412bb7124088b54cc4b6dd756c66
Sawada <105200073+SawadaAndzen@users.noreply.github.com>
2024-11-10 11:40:19 +0200

    init v241110

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from .models import Task
 
 
@@ -6,4 +8,56 @@ class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'status', 'priority', 'deadline']
-        widgets = {"deadline": forms.DateTimeInput(attrs = {'type': 'datetime-local'})}
+        widgets = {'deadline': forms.DateTimeInput(attrs = {'type': 'datetime-local', 'class': 'form-control'})}
+        
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+
+        self.fields['name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['description'].widget.attrs.update({'class': 'form-control'})
+        self.fields['status'].widget.attrs.update({'class': 'form-control'})
+        self.fields['priority'].widget.attrs.update({'class': 'form-control'})
+        
+        
+class CustomSignUpForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "first_name", "last_name", "password1", "password2"]
+        
+    def __init__(self, *args, **kwargs):
+        super(CustomSignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs.update({'class': 'form-control'})
+        self.fields['first_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['last_name'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
+        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+ 
+ 
+class CustomLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(CustomLoginForm, self).__init__(*args, **kwargs)
+        
+        self.fields['username'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Username'})
+        self.fields['password'].widget.attrs.update({'class': 'form-control', 'placeholder': 'Password'})
+        
+        
+class TaskFilterForm(forms.Form):
+    status_choices = [
+        ("", "All"),
+        ("_in_progress", 'In Progress'), 
+        ("_done", "Done"), 
+        ("_expired", "Expired")
+    ]
+    
+    priority_choices = [
+        ("", "All"),
+        ("_low", "Low"), 
+        ("_mid", "Middle"), 
+        ("_high", "High")
+    ]
+    
+    status = forms.ChoiceField(choices = status_choices, required = False, label = "Status",  widget = forms.Select(attrs={'class': 'form-control'}))
+    priority = forms.ChoiceField(choices = priority_choices, required = False, label = "Priority", widget = forms.Select(attrs={'class': 'form-control'}))
+    
+    
--- project/urls.py
+++ project/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from app.views import SignUp
+from app.forms import CustomLoginForm
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("app.urls"))
+    path('', include("app.urls")),
+    path("signup/", SignUp.as_view(), name = "signup"),
+    path("login/", LoginView.as_view(template_name = "app/auth/login.html", authentication_form = CustomLoginForm), name = "login"),
+    path("logout/", LogoutView.as_view(), name = "logout"),
 ]
